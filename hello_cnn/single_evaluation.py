c8c148f2de60dbbb480fca971a93265b69e35c05
n0psledbyte <n0psledbyte@gmail.com>
2020-04-12 16:50:44 +0700

    update single_evaluation.py

--- hello_cnn/single_evaluation.py
+++ hello_cnn/single_evaluation.py
@@ -5,7 +5,7 @@ import sys
 MAX_NAME = 24 # max name length
 name = "xi jinping"
 if len(sys.argv) > 1:
-    name = sys.argv[0][:MAX_NAME]
+    name = sys.argv[1][:MAX_NAME]
 X = list(map(lambda i: float(ord(i)), name.lower().ljust(24, "\x00")))
 X = np.array(X).reshape(24, 1)
 
@@ -14,12 +14,11 @@ loaded_model_json = json_file.read()
 json_file.close()
 loaded_model = model_from_json(loaded_model_json)
 loaded_model.load_weights("model.h5")
-print("Loaded model from disk")
 
 loaded_model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 res = loaded_model.predict(np.array([X,]))
-if res < 0.5:
+if bool(res < 0.5):
     print("This is chinese name")
 else:
     print("This is russian name")
